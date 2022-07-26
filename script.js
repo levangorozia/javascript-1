77b6d722f34997b90ed031ee689fc55807925f6b
levangorozia <106963464+levangorozia@users.noreply.github.com>
2022-07-26 17:05:23 +0400

    add javascript

---
+++ script.js
@@ -0,0 +1,72 @@
+// davaleba #1
+
+// let  user = {
+//     firstname:'giorgi',
+//     lastname: 'smith',
+//     age: 25,
+//     studentstatus: 'active'
+//     };
+
+//     console.log(user.studentstatus);
+    
+
+// davaleba #2
+
+// let user = ['giorgi','smith', 25, 'active']
+
+// for (let items of user) {
+
+//     console.log(items);
+// }
+
+
+
+// davaleba #3  ------------
+
+// let numbers = [5, 6, 10, 25, 3, 7, 4, 100, 28, 36, 89];
+
+// for (let items of numbers){
+
+//     if (numbers > 5)
+//     console.log(items);
+// }
+
+
+// (let x=0; x < names.length; x++ ) {
+
+// console.log(numbers.for);
+
+// }
+
+
+
+
+// [5, 6, 10, 25, 3, 7, 4, 100, 28, 36, 89];
+
+
+let numbers = 10;
+
+ if (numbers > 5) {
+  console.log('hello');
+
+} else if (numbers > 100) {
+    console.log('hello2');
+} else {
+    console.log('error');
+}
+
+
+
+
+// davaleba #4  ------------
+
+
+// davaleba #5  
+
+// let array = ['watermelon', 'pear', 10, 45, 50, 'apple', 'ananas'] 
+
+// for (let items of array){
+//     console.log(string);
+// }
+
+
